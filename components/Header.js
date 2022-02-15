32b1b7d65c387a396a26b0bc0d22ab89d6519070
lenore wei <weirong9412@gmail.com>
2022-02-15 22:04:21 +0800

    update

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import BLOG from '@/blog.config'
 import { useLocale } from '@/lib/locale'
 
@@ -22,7 +23,7 @@ const NavBar = () => {
                 className="block ml-4 text-black dark:text-gray-50 nav"
               >
                 <Link href={link.to}>
-                  <a>{link.name}</a>
+                  <a target={link.id === 2?'_blank':''}>{link.name}</a>
                 </Link>
               </li>
             )
@@ -70,33 +71,11 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
-                <svg
+                <Image
                   width="24"
                   height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect
-                    width="24"
-                    height="24"
-                    className="fill-current text-black dark:text-white"
-                  />
-                  <rect width="24" height="24" fill="url(#paint0_radial)" />
-                  <defs>
-                    <radialGradient
-                      id="paint0_radial"
-                      cx="0"
-                      cy="0"
-                      r="1"
-                      gradientUnits="userSpaceOnUse"
-                      gradientTransform="rotate(45) scale(39.598)"
-                    >
-                      <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                      <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                    </radialGradient>
-                  </defs>
-                </svg>
+                  src="https://picture-1254182071.cos.ap-beijing.myqcloud.com/photo/android-chrome-512x512.png"
+                />
               </div>
             </a>
           </Link>
