9a33af0ac8a1788be0e58e8b525d194f1c63d923
lenore wei <weirong9412@gmail.com>
2022-02-15 22:19:02 +0800

    change

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import BLOG from '@/blog.config'
 import { useLocale } from '@/lib/locale'
 
@@ -71,7 +70,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
           <Link href="/">
             <a aria-label={BLOG.title}>
               <div className="h-6">
-                <Image
+                <img
                   width="24"
                   height="24"
                   src={'https://picture-1254182071.cos.ap-beijing.myqcloud.com/photo/android-chrome-512x512.png'}
